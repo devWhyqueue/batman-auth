1592ca69959bda19648108bd07da71f99c2e5774
Queisler, Yannik <Yannik.Queisler@kvwl.de>
2020-03-03 16:35:32 +0100

    Add test data

--- src/main/java/de/devwhyqueue/authservice/resource/UserResource.java
+++ src/main/java/de/devwhyqueue/authservice/resource/UserResource.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,8 +36,20 @@ public class UserResource {
     this.userRepository = userRepository;
   }
 
+  @GetMapping("/users/{id}")
+  public ResponseEntity<User> getBasicUserInfoById(@PathVariable Long id) {
+    log.debug("REST request to get user with id {}", id);
+    return this.userRepository.findById(id)
+        .map(user -> {
+          user.setEmail("[PROTECTED]");
+          user.setPassword("[PROTECTED]");
+          return ResponseEntity.ok(user);
+        })
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+  }
+
   @GetMapping("/users/self")
-  public ResponseEntity<User> getUser() {
+  public ResponseEntity<User> getOwnUser() {
     log.debug("REST request to get own user");
     return userService.getUserWithAuthorities()
         .map(user -> {
--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -1,6 +1,21 @@
 DELETE FROM User_Authority;
 DELETE FROM Batman_User;
 DELETE FROM Authority;
+
 INSERT INTO Authority VALUES ('ROLE_USER');
 INSERT INTO Authority VALUES ('ROLE_ANONYMOUS');
 INSERT INTO Authority VALUES ('ROLE_ADMIN');
+
+INSERT INTO Batman_User (id, email, password_hash, first_name, last_name, gender, club) VALUES (1, 'max@a.de', '$2a$10$5jTV8V7ovQe2KeVZOTam..5.dxaoJgDMHLWpqJGh/HKqwcl3xpuue', 'Max', 'Mustermann', 0, 'SV Musterstadt');
+INSERT INTO Batman_User (id, email, password_hash, first_name, last_name, gender, club) VALUES (2, 'marius@a.de', '$2a$10$5jTV8V7ovQe2KeVZOTam..5.dxaoJgDMHLWpqJGh/HKqwcl3xpuue', 'Marius', 'Mustermann', 0, 'SV Musterstadt');
+INSERT INTO Batman_User (id, email, password_hash, first_name, last_name, gender, club) VALUES (3, 'maja@a.de', '$2a$10$5jTV8V7ovQe2KeVZOTam..5.dxaoJgDMHLWpqJGh/HKqwcl3xpuue', 'Maja', 'Mustermann', 1, 'SV Musterstadt');
+INSERT INTO Batman_User (id, email, password_hash, first_name, last_name, gender, club) VALUES (4, 'maren@a.de', '$2a$10$5jTV8V7ovQe2KeVZOTam..5.dxaoJgDMHLWpqJGh/HKqwcl3xpuue', 'Maren', 'Mustermann', 1, 'SV Musterstadt');
+INSERT INTO Batman_User (id, email, password_hash, first_name, last_name, gender, club) VALUES (9, 'mark@a.de', '$2a$10$5jTV8V7ovQe2KeVZOTam..5.dxaoJgDMHLWpqJGh/HKqwcl3xpuue', 'Mark', 'Müller', 0, 'SV Müllersbach');
+INSERT INTO Batman_User (id, email, password_hash, first_name, last_name, gender, club) VALUES (10, 'maria@a.de', '$2a$10$5jTV8V7ovQe2KeVZOTam..5.dxaoJgDMHLWpqJGh/HKqwcl3xpuue', 'Maria', 'Müller', 1, 'SV Müllersbach');
+
+INSERT INTO User_Authority (user_id, authority_name) VALUES (1, 'ROLE_USER');
+INSERT INTO User_Authority (user_id, authority_name) VALUES (2, 'ROLE_USER');
+INSERT INTO User_Authority (user_id, authority_name) VALUES (3, 'ROLE_USER');
+INSERT INTO User_Authority (user_id, authority_name) VALUES (4, 'ROLE_USER');
+INSERT INTO User_Authority (user_id, authority_name) VALUES (9, 'ROLE_USER');
+INSERT INTO User_Authority (user_id, authority_name) VALUES (10, 'ROLE_USER');
