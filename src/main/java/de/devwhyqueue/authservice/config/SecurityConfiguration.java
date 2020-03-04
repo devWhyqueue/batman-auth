ae5a5b06191a395d87d4efec9dbed72704bd5f98
devWhyqueue <dev.yannik.queisler@gmail.com>
2020-03-05 00:01:13 +0100

    Allow preflight requests

--- src/main/java/de/devwhyqueue/authservice/config/SecurityConfiguration.java
+++ src/main/java/de/devwhyqueue/authservice/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -41,7 +42,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // @formatter:on
   }
 
-
+  @Override
+  public void configure(WebSecurity web) {
+    web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+  }
+  
   private JWTConfigurer securityConfigurerAdapter() {
     return new JWTConfigurer(tokenProvider);
   }
